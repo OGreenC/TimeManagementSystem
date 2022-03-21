a2f5b6a76fa85cab50a8c2205b704f46e8412afe
OGreenC <46462998+OGreenC@users.noreply.github.com>
2022-03-21 15:21:30 +0100

    Initial commit with Main class
    With a working connection between tests and Business logic.

---
+++ src/main/java/dtu/timeManagement/app/TimeManagementApp.java
@@ -0,0 +1,10 @@
+package dtu.timeManagement.app;
+
+public class TimeManagementApp {
+
+    public void sendMessage() {
+        System.out.println("Den her linje bliver kørt fra step Definitions - YaY");
+    }
+
+
+}
--- src/test/java/dtu/calculator/StepDefinitions.java
+++ src/test/java/dtu/calculator/StepDefinitions.java
@@ -2,13 +2,22 @@ package dtu.calculator;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import dtu.timeManagement.app.TimeManagementApp;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class StepDefinitions {
+
+	private TimeManagementApp timeManagementApp;
+
+
+	public StepDefinitions(TimeManagementApp timeManagementApp) {
+		this.timeManagementApp = timeManagementApp;
+	}
+
 	@When("I do nothing")
 	public void i_do_nothing() {
-	    
+	    timeManagementApp.sendMessage();
 	}
 
 	@Then("everything is okay")
